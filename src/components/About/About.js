e04f02dc923a95d380e6a9d6b7f024b7633eca68
franco caspani <francocasp@gmail.com>
2022-08-18 12:51:15 -0300

    update about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -17,8 +17,15 @@ function About() {
             src={img}
             alt="man-svgrepo"
           />
-          <div className="AboutBio">
+          {/* <div className="AboutBio">
             Hello! My name is <strong>Franco Caspani</strong> I am a Full Stack MERN developer and an electromechanical technician. I had a few years doing system engineering, where I learned the basis of logical thinking. Then, I traveled and lived in different countries, something that made me grow since I exposed myself to constant changes. The adrenaline of living in a new country, where there are new rules and a different way to live life, it’s a challenge that I’m always happy to face.
+          </div> */}
+          <div className="AboutBio">
+            <p>
+              Hello! My name is <strong>Franco Caspani</strong> I’m a fast learner, capable, and hard-working full stack developer with special attention to detail, enthusiastic about getting into the IT world and delivering quality software.
+
+After years of traveling, where I realized all my potential and flexibility to live and work in such different cultures and industries and become confident in all of them, I transitioned to software engineering to become a master of it.
+            </p>
           </div>
           <div className="AboutBio tagline2">
             I have become confident using the following technologies.
